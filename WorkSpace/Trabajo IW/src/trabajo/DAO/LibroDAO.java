dea41fc3e5716477e6311afc69a00d2a0e1c641d
i62pecar <48968165+i62pecar@users.noreply.github.com>
2020-05-27 16:50:23 +0200

    Update LibroDAO.java

--- WorkSpace/Trabajo IW/src/trabajo/DAO/LibroDAO.java
+++ WorkSpace/Trabajo IW/src/trabajo/DAO/LibroDAO.java
@@ -1,4 +1,3 @@
-
 package trabajo.DAO;
 
 import java.io.BufferedWriter;
@@ -8,6 +7,9 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.Scanner;
 
+/**
+ * Clase que permite modificar y establecer conexion con los libros en la base de datos
+ */
 public class LibroDAO {
 	private int id_libro;
 	private String Titulo;
@@ -18,49 +20,110 @@ public class LibroDAO {
 	private int Encuadernamiento;
 	private int Reservado;
 	
+	/**
+	 * Observador de id_libro
+	 * @return Identificador de Libro
+	 */
 	public int getId_libro() {
 		return id_libro;
 	}
+	/**
+	 * Modificador de id_Libro
+	 * @param id_libro
+	 */
 	public void setId_libro(int id_libro) {
 		this.id_libro = id_libro;
 	}
+	/**
+	 * Observador de Titulo
+	 * @return Titulo del libro
+	 */
 	public String getTitulo() {
 		return Titulo;
 	}
+	/**
+	 * Modificador de Titulo
+	 * @param titulo
+	 */
 	public void setTitulo(String titulo) {
 		Titulo = titulo;
 	}
+	/**
+	 * Observador de Autor
+	 * @return Autor de Libro
+	 */
 	public String getAutor() {
 		return Autor;
 	}
+	/**
+	 * Modificador de Autor
+	 * @param autor
+	 */
 	public void setAutor(String autor) {
 		Autor = autor;
 	}
+	/**
+	 * Observador de  Editorial
+	 * @return Editorial de Libro
+	 */
 	public String getEditorial() {
 		return Editorial;
 	}
+	/**
+	 * Modificador de Editorial
+	 * @param editorial
+	 */
 	public void setEditorial(String editorial) {
 		Editorial = editorial;
 	}
+	/**
+	 * Observador de Materia
+	 * @return Materia de Libro
+	 */
 	public String getMateria() {
 		return Materia;
 	}
+	/**
+	 * Modificador de Materia
+	 * @param materia
+	 */
 	public void setMateria(String materia) {
 		Materia = materia;
 	}
+	/**
+	 * Observador de Paginas
+	 * @return Paginas de Libro
+	 */
 	public int getPaginas() {
 		return Paginas;
 	}
+	/**
+	 * Modificador de Paginas
+	 * @param paginas
+	 */
 	public void setPaginas(int paginas) {
 		Paginas = paginas;
 	}
+	/**
+	 * Observador de Encuadernamiento
+	 * @return Encuadernamiento de Libro
+	 */
 	public int getEncuadernamiento() {
 		return Encuadernamiento;
 	}
+	/**
+	 * Modificador de Encuadernamiento	
+	 * @param encuadernamiento
+	 */
 	public void setEncuadernamiento(int encuadernamiento) {
 		Encuadernamiento = encuadernamiento;
 	}
-
+	
+	/**
+	 * Carga en la web un libro aleatorio para su visualizacion
+	 * @param random
+	 * @return True si se puede cargar el libro, False si no se puede
+	 */
 	public boolean load_libro_aleatorio(int random) {
 
 		File f1 = new File("D:\\unaif\\Documents\\Universidad\\3.2\\IW\\Trabajo\\Trabajo\\IW_Bien\\WorkSpace\\Trabajo IW\\WebContent\\Ficheros\\Libros.csv");
@@ -92,7 +155,12 @@ public class LibroDAO {
 		  }
 		  return false;
 	}
-
+	
+	/**
+	 * Carga en la web un libro para su visualizacion
+	 * @param Nombre
+	 * @return True si se puede cargar el libro, False si no se puede
+	 */
 	public boolean load_libro(String Nombre) {
 		File f1 = new File("D:\\unaif\\Documents\\Universidad\\3.2\\IW\\Trabajo\\Trabajo\\IW_Bien\\WorkSpace\\Trabajo IW\\WebContent\\Ficheros\\Libros.csv");
 		if(f1.exists()) {
@@ -125,6 +193,19 @@ public class LibroDAO {
 	  return false;
 	}
 
+	/**
+	 * Guarda un libro en nuestra base de datos
+	 * @param id
+	 * @param titulo
+	 * @param autor
+	 * @param materia
+	 * @param editorial
+	 * @param paginas
+	 * @param encuadernamiento
+	 * @param reservado
+	 * @return True si se ha podido el libro, False si no se puede
+	 * @throws IOException
+	 */
 	public boolean guardarLibro(int id, String titulo, String autor, String materia, String editorial, int paginas, int encuadernamiento, int reservado) throws IOException{
 
   	  String data = id + "," + titulo + "," + autor + "," + materia + "," + editorial + "," + paginas + "," + encuadernamiento + "," + reservado;
@@ -142,7 +223,10 @@ public class LibroDAO {
   	return false;
  	}
 
-	
+	/**
+	 * Calcula el numero de libros en nuestra base de datos
+	 * @return Numero de libros
+	 */
 	public int numeroLibros() {
 		  File f1 = new File("D:\\unaif\\Documents\\Universidad\\3.2\\IW\\Trabajo\\Trabajo\\IW_Bien\\WorkSpace\\Trabajo IW\\WebContent\\Ficheros\\Libros.csv");
 		  if(f1.exists()) {
@@ -163,9 +247,17 @@ public class LibroDAO {
 	  }
 		return -1;
 	}
+	/**
+	 * Observador de Reservado
+	 * @return Reservador de Libro
+	 */
 	public int getReservado() {
 		return Reservado;
 	}
+	/**
+	 * Modificador de Reservado
+	 * @param reservado
+	 */
 	public void setReservado(int reservado) {
 		Reservado = reservado;
 	}
